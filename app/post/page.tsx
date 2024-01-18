ed6ecf9eab35ac2cbcc4376404b7d02294a6b52a
kelvin <muhammadfalahalfarisi@gmail.com>
2024-01-18 08:16:34 +0800

    Memindahkan server side ke client side

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import styles from './postpage.module.css'
 
 const Posts = () =>  {
     return (
-        <div className={styles.bgRed}>Post Page </div>
-    )
+
+        <>
+         <div className={styles.bgRed}>Post Page </div>
+         <button onClick={() => console.log('Lihat User')}>Lihat user</button>
+        </>
+       
+        )
 }
 
 export default Posts
